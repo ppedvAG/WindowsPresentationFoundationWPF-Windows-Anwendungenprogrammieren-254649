f65badac10bbb66b820e81af3c325230bcc2d0b0
ppedvLukasK <lukask@ppedv.de>
2025-07-08 11:37:35 +0200

    M006

---
+++ M006/Person.cs
@@ -0,0 +1,18 @@
+﻿namespace M006;
+
+public class Person
+{
+	public int ID { get; set; }
+
+	public string Name { get; set; }
+
+	public string Description { get; set; }
+
+	public string Author { get; set; }
+
+	public string Title { get; set; }
+
+	public string Url { get; set; }
+
+	public Person() { }
+}
