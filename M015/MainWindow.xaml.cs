dee3ff9d71ed68b9174617768aa147c66992402f
ppedvLukasK <lukask@ppedv.de>
2025-07-10 12:39:11 +0200

    M015

---
+++ M015/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using System.Windows;
+
+namespace M015;
+
+public partial class MainWindow : Window
+{
+	public MainWindow()
+	{
+		InitializeComponent();
+	}
+
+	private void Button_Click(object sender, RoutedEventArgs e)
+	{
+		//if (Thread.CurrentThread.CurrentUICulture.Name != "en-US")
+		//	Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-US");
+		//else
+		//	Thread.CurrentThread.CurrentUICulture = new CultureInfo("de-DE");
+
+		///////////////////////////////////////////////////////////////////////////////////////
+
+		string path = "pack://application:,,,/Localisation/Strings-#.xaml";
+		Uri uri = new Uri(path.Replace("#", Sprachauswahl.Text));
+
+		ResourceDictionary rd = new();
+		rd.Source = uri;
+
+		App.Current.Resources.MergedDictionaries.Clear();
+		App.Current.Resources.MergedDictionaries.Add(rd);
+	}
+}
